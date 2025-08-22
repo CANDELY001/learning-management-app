bc46c96ec3ca897ab2f9347d59ceb93ce5d24fd5
CandelyStack <candely1charifa@gmail.com>
2025-08-22 15:07:29 +0100

    Initial Commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import Providers from "./providers";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  display: "swap",
+});
+export const metadata: Metadata = {
+  title: "Learning App",
+  description: "Learning Management System built with Next.js and Tailwind CSS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${dmSans.className}`}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
