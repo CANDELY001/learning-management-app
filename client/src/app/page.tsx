d727939c9b317ed54e725c1f80efbde47804980e
CandelyStack <candely1charifa@gmail.com>
2025-08-22 17:53:06 +0100

    Frontend: Navbar & Landing section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
 import NoneDashboardNavbar from "@/components/NoneDashboardNavbar";
+import Landing from "./(nondashboard)/page";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NoneDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
     </div>
   );
 }
